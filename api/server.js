ae846544f9227819f8915f6bea068b97dbe55857
daveKidd <david.kidd@lambdaschool.com>
2021-02-09 11:55:22 -0700

    ketchup

--- api/server.js
+++ api/server.js
@@ -3,30 +3,14 @@
 // BREAK UP THIS MONOLITHIC FILE USING ROUTES
 const express = require('express');
 const adopterRouter = require("./adopters/adopters-router.js")
+const dogsRouter = require("./dogs/dogs-router")
 
 const server = express();
 
 server.use(express.json());
 server.use("/api/adopters", adopterRouter)
+server.use("/api/dogs", dogsRouter)
 
-const Dog = require('./dogs/dogs-model');
-
-
-// DOGS ENDPOINTS
-// DOGS ENDPOINTS
-// DOGS ENDPOINTS
-server.get('/api/dogs', (req, res) => {
-  Dog.find()
-    .then(dogs => {
-      res.status(200).json(dogs);
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).json({
-        message: 'Error retrieving the dogs',
-      });
-    });
-});
 
 // OTHER ENDPOINTS
 // OTHER ENDPOINTS
